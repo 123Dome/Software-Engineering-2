48d991400c18ad381ff02847296c7c8af6c0ca64
Dome <dominik.puck@web.de>
2025-05-07 13:34:17 +0200

    Alles zu JDBC gelöscht, da JPA genutzt wird.

--- src/main/java/org/hbrs/se2/project/hellocar/control/LoginControl.java
+++ src/main/java/org/hbrs/se2/project/hellocar/control/LoginControl.java
@@ -1,10 +1,8 @@
 package org.hbrs.se2.project.hellocar.control;
 
 import org.hbrs.se2.project.hellocar.control.exception.DatabaseUserException;
-import org.hbrs.se2.project.hellocar.dao.UserDAO;
 import org.hbrs.se2.project.hellocar.dtos.UserDTO;
 import org.hbrs.se2.project.hellocar.repository.UserRepository;
-import org.hbrs.se2.project.hellocar.services.db.exceptions.DatabaseLayerException;
 import org.hbrs.se2.project.hellocar.util.Globals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,10 +18,10 @@ public class LoginControl {
 
     public boolean authentificate(String username, String password ) throws DatabaseUserException {
         // Standard: User wird mit Spring JPA ausgelesen (Was sind die Vorteile?)
-        // UserDTO tmpUser = this.getUserWithJPA( username , password );
+        UserDTO tmpUser = this.getUserWithJPA( username , password );
 
         // Alternative: Auslesen des Users mit JDBC (Was sind die Vorteile bzw. Nachteile?)
-        UserDTO tmpUser = this.getUserWithJDBC( username , password );
+        // UserDTO tmpUser = this.getUserWithJDBC( username , password );
 
         if ( tmpUser == null ) {
             // ggf. hier ein Loggin einfügen
@@ -36,37 +34,6 @@ public class LoginControl {
     public UserDTO getCurrentUser(){
         return this.userDTO;
 
-    }
-
-    private UserDTO getUserWithJDBC( String username , String password ) throws DatabaseUserException {
-        UserDTO userTmp = null;
-        UserDAO dao = new UserDAO();
-        try {
-            userDTO = dao.findUserByUseridAndPassword( username , password );
-        }
-        catch ( DatabaseLayerException e) {
-
-            // Analyse und Umwandlung der technischen Errors in 'lesbaren' Darstellungen
-            // Durchreichung und Behandlung der Fehler (Chain Of Responsibility Pattern (SE-1))
-            String reason = e.getReason();
-
-            if (reason.equals(Globals.Errors.NOUSERFOUND)) {
-                return userTmp;
-                // throw new DatabaseUserException("No User could be found! Please check your credentials!");
-            }
-            else if ( reason.equals((Globals.Errors.SQLERROR))) {
-                throw new DatabaseUserException("There were problems with the SQL code. Please contact the developer!");
-            }
-            else if ( reason.equals((Globals.Errors.DATABASE ) )) {
-                throw new DatabaseUserException("A failure occured while trying to connect to database with JDBC. " +
-                        "Please contact the admin");
-            }
-            else {
-                throw new DatabaseUserException("A failure occured while");
-            }
-
-        }
-        return userDTO;
     }
 
     private UserDTO getUserWithJPA( String username , String password ) throws DatabaseUserException {
