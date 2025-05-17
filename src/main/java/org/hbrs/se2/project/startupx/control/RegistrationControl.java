e35121864f3afc7ac3af2b4b414f6ccbb54465ab
Dominik Puck <dominik.puck@smail.inf.h-brs.de>
2025-05-17 12:53:58 +0200

    Jeder neu registrierte Nutzer soll erstmal nur die Rolle User haben

--- src/main/java/org/hbrs/se2/project/startupx/control/RegistrationControl.java
+++ src/main/java/org/hbrs/se2/project/startupx/control/RegistrationControl.java
@@ -2,8 +2,10 @@ package org.hbrs.se2.project.startupx.control;
 
 import org.hbrs.se2.project.startupx.control.exception.RegistrationException;
 import org.hbrs.se2.project.startupx.dtos.UserDTO;
+import org.hbrs.se2.project.startupx.entities.Rolle;
 import org.hbrs.se2.project.startupx.entities.User;
 import org.hbrs.se2.project.startupx.mapper.UserMapper;
+import org.hbrs.se2.project.startupx.repository.RolleRepository;
 import org.hbrs.se2.project.startupx.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ public class RegistrationControl {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    private RolleRepository rolleRepository;
 
     public void registerUser(UserDTO userDTO) {
         // E-Mail darf nicht existieren
@@ -25,6 +29,14 @@ public class RegistrationControl {
         }
 
         User newUser = UserMapper.INSTANCE.mapToUser(userDTO);
+
+        // Jeder User hat erstmal die Standardrolle User
+        Rolle defaultRolle = rolleRepository.findByBezeichnung("user");
+        if (defaultRolle == null) {
+            throw new RegistrationException("Rolle 'user' nicht gefunden.");
+        }
+        newUser.getRollen().add(defaultRolle);
+
         userRepository.save(newUser);
     }
 
--- src/main/java/org/hbrs/se2/project/startupx/repository/RolleRepository.java
+++ src/main/java/org/hbrs/se2/project/startupx/repository/RolleRepository.java
@@ -4,4 +4,5 @@ import org.hbrs.se2.project.startupx.entities.Rolle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RolleRepository extends JpaRepository<Rolle, Long> {
+    Rolle findByBezeichnung(String bezeichnung);
 }
